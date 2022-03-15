d64c4bf511c82ebfc12499629e88fdad284efed8
Taiga <qqtaiga@gmail.com>
2022-03-15 23:36:58 +0800

    server: add GbfConnectStream background task

---
+++ server/src/GbfRaidFinder/BackgroundTasks/GbfConnectStreamTask.cs
@@ -0,0 +1,86 @@
+using GbfRaidFinder.Models;
+using GbfRaidFinder.Services;
+
+namespace GbfRaidFinder.BackgroundTasks;
+
+public class GbfConnectStreamTask : BackgroundService
+{
+    private readonly ILogger<GbfConnectStreamTask> _log;
+    private readonly IGbfRaidService _gbfRaidService;
+    private readonly IInMemBossesService _inMemService;
+    private readonly ITwitterFilteredStreamService _twitterFSService;
+
+    public GbfConnectStreamTask(ILogger<GbfConnectStreamTask> log,
+        IGbfRaidService gbfRaidService,
+        IInMemBossesService inMemService,
+        ITwitterFilteredStreamService twitterFSService)
+    {
+        _log = log;
+        _gbfRaidService = gbfRaidService;
+        _inMemService = inMemService;
+        _twitterFSService = twitterFSService;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Task.Yield();
+
+        await foreach (var tweet in _twitterFSService.ConnectStreamAsync())
+        {
+            try
+            {
+                var req = _gbfRaidService.ConvertGbfHelpTweetToRequest(tweet);
+                var raidBosses = _inMemService.Bosses.Values;
+                bool isNameExist = false;
+                for (int i = 0; i < raidBosses.Count(); i++)
+                {
+                    var boss = raidBosses.ElementAt(i);
+                    if (req.Lang == Language.English
+                        ? boss.EngName == req.BossName
+                        : boss.JapName == req.BossName)
+                    {
+                        isNameExist = true;
+                        _inMemService.AddRaidCode(boss.PerceptualHash,
+                            new(req.RaidCode, req.CreatedAt));
+                        // TODO: add send latest code to client
+
+                        break;
+                    }
+                }
+
+                if (isNameExist)
+                    continue;
+
+                var perceptualHash = await
+                    _gbfRaidService.GetImagePerceptualHashAsync(req.ImageUrl);
+
+                if (_inMemService.Bosses.ContainsKey(perceptualHash))
+                {
+                    var boss = _inMemService.Bosses[perceptualHash];
+                    if (req.Lang == Language.English)
+                        boss.EngName = req.BossName;
+                    else
+                        boss.JapName = req.BossName;
+
+                    _inMemService.AddRaidCode(boss.PerceptualHash,
+                        new(req.RaidCode, req.CreatedAt));
+                    // TODO: add send latest code to client
+                }
+                else
+                {
+                    GbfRaidBoss newBoss = new(perceptualHash, new(req.RaidCode, req.CreatedAt));
+                    if (req.Lang == Language.English)
+                        newBoss.EngName = req.BossName;
+                    else
+                        newBoss.JapName = req.BossName;
+
+                    _inMemService.AddRaidBoss(newBoss);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                _log.LogWarning(ex, tweet.Data.Text + " =>");
+            }
+        }
+    }
+}
